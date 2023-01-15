eea88bac9213630d81bc7b9d75962530a4389a4b
SANDIP CHITALE <sandipchitale@gmail.com>
2023-01-15 14:21:08 -0800

    .

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 
 import * as vscode from 'vscode';
 import * as k8s from 'vscode-kubernetes-tools-api';
+import { X509Certificate } from 'crypto';
+
 
 let ingressNginxDeploymentNamespace = 'ingress-nginx-ns';
 let ingressNginxDeploymentName = 'ingress-nginx-controller';
@@ -43,12 +45,13 @@ async function backends(target?: any) {
     if (commandTarget) {
         if (commandTarget.nodeType === 'resource') {
             if (commandTarget.resourceKind.manifestKind === 'Ingress') {
-                const backendDetails = await kubectl.api.invokeCommand(`ingress-nginx backends -n ${ingressNginxDeploymentNamespace} --deployment ${ingressNginxDeploymentName}`);
-                if (backendDetails && backendDetails.stdout) {
-                    const doc = await vscode.workspace.openTextDocument({
-                        content: `Ingress Backends:\n\n${backendDetails.stdout}`
-                    });
-                    await vscode.window.showTextDocument(doc, { preview: false });
+                try {
+                    invokeCommandAndShowOutput(
+                        `ingress-nginx backends -n ${ingressNginxDeploymentNamespace} --deployment ${ingressNginxDeploymentName}`,
+                        `Ingress Backends:`
+                    );
+                } catch (error) {
+                    vscode.window.showErrorMessage(`Failed to get ingress backends.`);
                 }
             }
         }
@@ -76,11 +79,13 @@ async function hostConf(target?: any) {
                     ingressDetailsAsJson.spec.rules.forEach(async (rule: any) => {
                         if (rule.host) {
                             const confDetails = await kubectl.api.invokeCommand(`ingress-nginx conf -n ${ingressNginxDeploymentNamespace} --deployment ${ingressNginxDeploymentName} --host ${rule.host}`);
-                            if (confDetails && confDetails.stdout) {
-                                const doc = await vscode.workspace.openTextDocument({
-                                    content: `Configuration for ingress: ${commandTarget.name} host: ${rule.host}\n\n${confDetails.stdout}`
-                                });
-                                await vscode.window.showTextDocument(doc, { preview: false });
+                            try {
+                                invokeCommandAndShowOutput(
+                                    `ingress-nginx conf -n ${ingressNginxDeploymentNamespace} --deployment ${ingressNginxDeploymentName} --host ${rule.host}`,
+                                    `Configuration for ingress: ${commandTarget.name} host: ${rule.host}`
+                                );
+                            } catch (error) {
+                                vscode.window.showErrorMessage(`Failed to get configuration for ingress: ${commandTarget.name} host: ${rule.host}`);
                             }
                         }
                     });
@@ -110,12 +115,14 @@ async function hostCerts(target?: any) {
                     const ingressDetailsAsJson = JSON.parse(ingressDetails.stdout);
                     ingressDetailsAsJson.spec.rules.forEach(async (rule: any) => {
                         if (rule.host) {
-                            const certsDetails = await kubectl.api.invokeCommand(`ingress-nginx certs -n ${ingressNginxDeploymentNamespace} --deployment ${ingressNginxDeploymentName} --host ${rule.host}`);
-                            if (certsDetails && certsDetails.stdout) {
-                                const doc = await vscode.workspace.openTextDocument({
-                                    content: `Certificates for ingress: ${commandTarget.name} host: ${rule.host}\n\n${certsDetails.stdout}`
-                                });
-                                await vscode.window.showTextDocument(doc, { preview: false });
+                            try {
+                                const certificatesAndKeys = await invokeCommandAndShowOutput(
+                                    `ingress-nginx certs -n ${ ingressNginxDeploymentNamespace } --deployment ${ ingressNginxDeploymentName } --host ${ rule.host }`,
+                                    `Certificates for ingress: ${commandTarget.name} host: ${rule.host}`,
+                                    true
+                                );
+                            } catch (error) {
+                                vscode.window.showErrorMessage(`Failed to get certificates for ingress: ${commandTarget.name} host: ${rule.host}: Error: ${error}`);
                             }
                         }
                     });
@@ -124,6 +131,40 @@ async function hostCerts(target?: any) {
         }
     }
 }
+
+async function invokeCommandAndShowOutput(command: string, prefix: string, processCerts = false): Promise<string> {
+    const kubectl = await k8s.extension.kubectl.v1;
+    if (!kubectl.available) {
+        return;
+    }
+
+    const commandDetails = await kubectl.api.invokeCommand(command);
+    if (commandDetails && commandDetails.code === 0 && commandDetails.stdout) {
+        let content = commandDetails.stdout;
+        if (processCerts) {
+            content = '';
+            commandDetails.stdout.split('-----END CERTIFICATE-----').forEach((certificateOrKey: string) => {
+                if (certificateOrKey) {
+                    if (certificateOrKey.startsWith('-----BEGIN CERTIFICATE-----')) {
+                        const certificateAsPem = `${certificateOrKey}-----END CERTIFICATE-----`.trim();
+                        const cert = new X509Certificate(certificateAsPem);
+                        content += `${content}\n${certificateAsPem}\n${cert.toString()}`;
+                    } else {
+                        content += `${content}\n${certificateOrKey}`;
+                    }
+                }
+            });
+        }
+        const doc = await vscode.workspace.openTextDocument({
+            content: `${prefix}:\n\n${content}`
+        });
+        await vscode.window.showTextDocument(doc, { preview: false });
+        return Promise.resolve(commandDetails.stdout);
+    } else {
+        return Promise.reject(commandDetails.stderr);
+    }
+}
+
 
 export function deactivate() {
 }
